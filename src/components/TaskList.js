6ed74746cd80c49598a59c7b3e6074e4c3a576c0
Mootda <de.qtner@gmail.com>
2019-10-17 01:03:11 +0800

    edit TaskList to map over tasks and show as list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,25 @@
+import './TaskList.css' 
 import React from 'react';
 
 const TaskList = (props) => {
-    const task = props.tasks;
-    return <div>{task}</div>
+    const taskTitle = props.tasks.map((task) => {
+        return(<div className="item">
+            <div className="right floated content">
+                <div className="circular ui basic icon button">
+                    <i class="times icon"></i>
+                </div>
+            </div>
+            <div className="content" key={task.id}>{task.title}</div>
+        </div>
+        )
+    });
+    return (
+    <div className="task-list">
+        <div className="ui relaxed divided list">
+            {taskTitle}
+        </div>
+    </div>
+    )
 }
 
 export default TaskList;
