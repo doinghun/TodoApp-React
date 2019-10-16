eeb4ddf433e25b7f7603b5e338cd3756ea645afa
Mootda <de.qtner@gmail.com>
2019-10-17 01:01:35 +0800

    add axios fetch task from api

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
+import './App.css'
 import React from 'react';
 import TaskAdd from './TaskAdd';
 import TaskList from './TaskList';
+import axios from 'axios';
 
 class App extends React.Component {
-    state = { task: "" }
+    state = { tasks: [] }
 
-    onTaskSubmit = (newTask) => {
-        this.setState({ task: newTask})
+    // onTaskSubmit = (newTask) => {
+    //     this.setState({ task: newTask})
+    // }
+
+    fetchTasks() {
+        axios.get('http://localhost:4000/api/tasks')
+        .then((res) => {
+            this.setState({ tasks: res.data });
+            console.log(this.state.tasks)
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+    }
+
+    componentDidMount() {
+        this.fetchTasks()
     }
 
     render(){
@@ -14,7 +31,7 @@ class App extends React.Component {
         <div className = "ui container" style={{marginTop : '10px'}}>
             <h2 align="center">To Do List in React</h2>
             <TaskAdd onSubmit={this.onTaskSubmit}/>
-            <TaskList tasks={this.state.task}/>
+            <TaskList tasks={this.state.tasks}/>
         </div>
         )
     }
