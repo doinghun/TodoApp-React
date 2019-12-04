2b40e6f3eff3d665c6b1120af506519b35d9d878
doinghun <de.qtner@gmail.com>
2019-12-05 00:53:49 +0800

    feat: add redux config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware, compose } from "redux"
+import reducers from "./reducers"
+import App from "./components/App"
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducers, composeEnhancers(applyMiddleware()))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+)
