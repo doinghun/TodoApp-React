a3d24d61b04c651f35db504bc54fef9435a444ce
doinghun <de.qtner@gmail.com>
2019-12-11 12:34:59 +0800

    fix api call baseurl 2

--- client/src/api/todo.js
+++ client/src/api/todo.js
@@ -1,3 +1,7 @@
 import axios from "axios"
+const port = process.env.PORT || 5000
+const baseURL = `http://localhost:${port}`
 
-export default axios.create({})
+export default axios.create({
+  baseURL: baseURL
+})
