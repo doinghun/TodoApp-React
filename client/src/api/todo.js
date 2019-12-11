ab35ced97dcc6232bbc4624d77d7ddcf19a000a5
doinghun <de.qtner@gmail.com>
2019-12-11 12:47:50 +0800

    fix api call baseurl for production

--- client/src/api/todo.js
+++ client/src/api/todo.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = "http://localhost:5000" || process.env.SERVER_URL
+const baseURL = process.env.SERVER_URL
 
 export default axios.create({
   baseURL: baseURL
